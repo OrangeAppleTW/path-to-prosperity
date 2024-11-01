61d0e038ff75e1f0c350afa079cafbacce204576
“lloyd3126” <lloyd3126@gmail.com>
2024-11-01 17:20:59 +0800

    fix: Resolve static asset loading issue

--- js/teacher.js
+++ js/teacher.js
@@ -11,7 +11,8 @@ $(document).ready(function () {
   let baseURL;
   if (
     window.location.hostname === '127.0.0.1' ||
-    window.location.hostname === 'localhost'
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === 'https://fq.orangeapple.co/'
   ) {
     baseURL = window.location.origin;
   } else {
