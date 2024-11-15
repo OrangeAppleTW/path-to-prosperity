b933e4965ea12d520ba37c200a95e431df242f75
“lloyd3126” <lloyd3126@gmail.com>
2024-11-15 10:58:13 +0800

    fix: Change layout

--- src/js/cardDrawer.js
+++ src/js/cardDrawer.js
@@ -81,7 +81,7 @@ export class CardDrawer {
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title text-dark" id="drawCardModalLabel">抽片介面</h5>
+                <h5 class="modal-title text-dark" id="drawCardModalLabel">抽卡介面</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body text-center">
